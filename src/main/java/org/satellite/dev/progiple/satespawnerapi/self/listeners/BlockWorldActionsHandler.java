842fe44dd3121b0752a73cb4274fcee4f59460d1
ProGiple <ghosthostgtop@gmail.com>
2025-04-02 17:09:17 +0300

    1.0.6

--- src/main/java/org/satellite/dev/progiple/satespawnerapi/self/listeners/BlockWorldActionsHandler.java
+++ src/main/java/org/satellite/dev/progiple/satespawnerapi/self/listeners/BlockWorldActionsHandler.java
@@ -2,16 +2,22 @@ package org.satellite.dev.progiple.satespawnerapi.self.listeners;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.novasparkle.lunaspring.API.Menus.MenuManager;
 import org.satellite.dev.progiple.satespawnerapi.SateSpawnerAPI;
 import org.satellite.dev.progiple.satespawnerapi.api.ISAPIComponent;
 import org.satellite.dev.progiple.satespawnerapi.api.SpawnerAPI;
 import org.satellite.dev.progiple.satespawnerapi.api.menu.SpawnerMenu;
 import org.satellite.dev.progiple.satespawnerapi.self.Config;
+
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class BlockWorldActionsHandler implements Listener {
     @EventHandler
@@ -36,12 +42,24 @@ public class BlockWorldActionsHandler implements Listener {
         ISAPIComponent component = Config.getBoolean("after_click_action.registerOnly") ?
                 api.getApi(Config.getString("after_click_action.id")) : null;
 
-        MenuManager.getActiveInventories().values()
+        new HashSet<>(MenuManager.getActiveInventories().values())
                 .forEach(i -> {
                     if (i instanceof SpawnerMenu spawnerMenu &&
                             spawnerMenu.getLocation().equals(block.getLocation())) spawnerMenu.getPlayer().closeInventory();});
 
         if (component == null) api.getValues().forEach(a -> a.onBreakSpawner(e));
         else component.onBreakSpawner(e);
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        event.blockList().removeIf(block -> block.getType() == Material.SPAWNER);
+    }
+
+    @EventHandler
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        if (event.getEntityType() == EntityType.WITHER && event.getBlock().getType() == Material.SPAWNER) {
+            event.setCancelled(true);
+        }
     }
 }
